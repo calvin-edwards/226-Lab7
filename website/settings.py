27a6154f4acc06b2c474ad8d838e3b16ccaa1680
Calvin Edwards <calvinl.edwards@gmail.com>
2019-11-10 18:58:20 -0800

    changed known hosts in settings.py

--- website/settings.py
+++ website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['https://fierce-retreat-66166.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
